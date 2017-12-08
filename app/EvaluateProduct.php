876fc3adf906c879292e63b6d3bf551a1610a43e
jonaselan <jonas.elan@gmail.com>
2017-12-08 11:56:41 -0300

    Add model for product evaluations

---
+++ app/EvaluateProduct.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace larastock;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EvaluateProduct extends Model
+{
+    protected $fillable = [
+      'product_id', 'note', 'comment'
+    ];
+
+    public function product(){
+    return $this->belongsTo('larastock\Product');
+    }
+}
--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
   protected $fillable = array('name', 'description', 'value', 'count');
+
+  public function evaluations(){
+    return $this->hasMany('larastock\EvaluateProduct');
+  }
 }
